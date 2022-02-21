d96e62fdd0d45d00a4e4a1294a60a45c76ec47f2
Aryan <aryangodara03@gmail.com>
2022-02-21 21:11:13 +0530

    lynting done

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,10 @@
-def add (x,y):
+def add(x, y):
     """Add two numbers together"""
+
     return x + y
 
-def subtract (x,y):
+
+def subtract(x, y):
     """Subtract x from y and return value"""
+
     return y - x
